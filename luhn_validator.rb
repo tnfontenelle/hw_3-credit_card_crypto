3a8b7fb7d4ad69ec369f6cff18a414bad5f6e928
tnfontenelle <tnfontenelle@hotmail.com>
2015-03-25 23:01:34 +0800

    First Commit

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -7,5 +7,25 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    
+    #Reversing the array and multiplying the value in every other index by 2
+    n_arr = nums_a.reverse.map.with_index{|n,i| i%2 == 0? n : n*2}
+
+    #adding up the digits in any number that has more than one digit
+    #the max number of digits is 2 because 9*2= 18 (i.e. 18 has two digits)
+    n_arr.map! do|n| 
+        if n > 9
+		num= n.to_s.chars.map(&:to_i)
+		n = num.first+num.last
+	else
+		n
+	end
+    end
+
+    #adding up all number
+    check_digit = 10 - (n_arr[1..n_arr.length - 1].reduce(:+)) % 10
+    
+    #implicit return to check whether check_digit is correct
+    check-digit == nums_a.last
   end
 end
