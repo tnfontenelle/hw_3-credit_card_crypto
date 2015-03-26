873c7eee325017ee4f2493bb2806b72dd1b6859c
tnfontenelle <tnfontenelle@hotmail.com>
2015-03-26 11:10:55 +0800

    Fourth Commit: Trying to save running time

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -10,18 +10,11 @@ module LuhnValidator
     # TODO: use the integers in nums_a to validate its last check digit
 
     # Reversing the array and multiplying the value in every other index by 2
-    n_arr = nums_a.reverse.map.with_index { |n, i| i % 2 == 0 ? n : n * 2 }
+    n_arr = nums_a.reverse.map.with_index { |n, i| i.even? ? n : n * 2 }
 
     # adding up the digits in any number that has more than one digit
-    # the max number of digits is 2 because 9*2= 18 (i.e. 18 has two digits)
-    n_arr.map! do|n|
-      if n > 9
-        n.to_s.chars.map(&:to_i).reduce(:+)
-      else
-        n
-      end
-    end
-    # adding up all number
+    n_arr.map! { |n| n > 9 ? n.to_s.chars.map(&:to_i).reduce(:+) : n }
+    # adding up all numbers
     check_digit = ((n_arr[1..n_arr.length - 1].reduce(:+)) * 9) % 10
 
     # implicit return to check whether check_digit is correct
