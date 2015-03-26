658d5d69334de5103b81033d6fbb9a3897b35a62
tnfontenelle <tnfontenelle@hotmail.com>
2015-03-26 11:27:46 +0800

    Fifth Commit: Trying to save running time

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -11,11 +11,12 @@ module LuhnValidator
 
     # Reversing the array and multiplying the value in every other index by 2
     n_arr = nums_a.reverse.map.with_index { |n, i| i.even? ? n : n * 2 }
+    n_arr.drop(1)
 
     # adding up the digits in any number that has more than one digit
     n_arr.map! { |n| n > 9 ? n.to_s.chars.map(&:to_i).reduce(:+) : n }
     # adding up all numbers
-    check_digit = ((n_arr[1..n_arr.length - 1].reduce(:+)) * 9) % 10
+    check_digit = (n_arr.reduce(:+) * 9) % 10
 
     # implicit return to check whether check_digit is correct
     check_digit == nums_a.last
