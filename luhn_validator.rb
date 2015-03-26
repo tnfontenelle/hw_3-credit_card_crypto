e8fac14a994d7e126a0d8e9462204f861c9c284c
tnfontenelle <tnfontenelle@hotmail.com>
2015-03-26 10:40:40 +0800

    Third Commit: Fixing rubocop errors

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,4 +1,4 @@
-# Luhn Module
+# LuhnValidator Module
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -8,23 +8,23 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
-    
-    #Reversing the array and multiplying the value in every other index by 2
-    n_arr = nums_a.reverse.map.with_index{|n,i| i%2 == 0? n : n*2}
+
+    # Reversing the array and multiplying the value in every other index by 2
+    n_arr = nums_a.reverse.map.with_index { |n, i| i % 2 == 0 ? n : n * 2 }
 
     # adding up the digits in any number that has more than one digit
     # the max number of digits is 2 because 9*2= 18 (i.e. 18 has two digits)
-    n_arr.map! do|n| 
+    n_arr.map! do|n|
       if n > 9
-	n.to_s.chars.map(&:to_i).reduce(:+)
+        n.to_s.chars.map(&:to_i).reduce(:+)
       else
         n
       end
     end
-   # adding up all number
-   check_digit = ((n_arr[1..n_arr.length - 1].reduce(:+)) * 9) % 10
-    
-   # implicit return to check whether check_digit is correct
-   check_digit == nums_a.last
+    # adding up all number
+    check_digit = ((n_arr[1..n_arr.length - 1].reduce(:+)) * 9) % 10
+
+    # implicit return to check whether check_digit is correct
+    check_digit == nums_a.last
   end
 end
