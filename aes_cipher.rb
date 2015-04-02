7bea6071b7edae3bd8babffc070e8c8ff7aa64e2
tnfontenelle <tnfontenelle@hotmail.com>
2015-04-02 16:22:33 +0800

    Completed AES cipher

--- aes_cipher.rb
+++ aes_cipher.rb
@@ -1,4 +1,5 @@
 require 'openssl'
+require 'json'
 
 module AesCipher
   def self.encrypt(document, key)
@@ -7,9 +8,22 @@ module AesCipher
     #       and ciphertext is the output of AES encryption
     # NOTE: Use hexadecimal strings for output so that it is screen printable
     #       Use cipher block chaining mode only!
+
+    cipher = OpenSSL::Cipher::AES.new(128, :CBC)
+    cipher.encrypt
+    cipher.key = key
+    initvec = cipher.random_iv
+    ciphertext = cipher.update(document) + cipher.final
+    [initvec.unpack("H*"),ciphertext.unpack("H*")].to_json
   end
 
-  def self.encrypt(aes_crypt, key)
+  def self.decrypt(aes_crypt, key)
     # TODO: decrypt from JSON output (aes_crypt) of encrypt method above
+    decipher = OpenSSL::Cipher::AES.new(128, :CBC)
+    decipher.decrypt
+    decipher.key = key
+    input = JSON.parse(aes_crypt)
+    decipher.iv = input[0].pack("H*")
+    decipher.update(input[1].pack("H*")) + decipher.final
   end
 end
--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -7,9 +7,45 @@ module DoubleTranspositionCipher
     # 3. sort rows in predictibly random way using key as seed
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
+    document.to_s # converting the document to a string
+    squarert = Math.sqrt(document.length)
+    row = squarert.round # finding number of rows & rounding it up
+    col = 0
+    if squarert == squarert.to_i
+      col = row
+    else
+      col = (document.length / row) + 1
+    end
+    # breaking up doc into a matrix that is almost a square
+    document = document.chars.to_a.each_slice(col).to_a
+    # randomly shuffling rows first, then columns within rows
+    document.shuffle!(random: Random.new(key))
+    document.map! { |d| d.shuffle(random.Random.new(key)) }.join
   end
 
   def self.decrypt(ciphertext, key)
     # TODO: FILL THIS IN!
+    ciphertext = ciphertext.to_s.chars.to_a # converting to array of chars
+    squarert = Math.sqrt(ciphertext.length)
+    row = squarert.round # finding number of rows & rounding it up
+    col = 0
+    if squarert == squarert.to_i
+      col = row
+    else
+      col = (ciphertext.length / row) + 1
+    end
+    # breaking up arr into a matrix that is almost a square
+    arr = (1..ciphertext.length).to_a.each_slice(col).to_a
+    # randomly shuffling rows first, then columns within rows
+    arr.shuffle!(random: Random.new(key))
+    arr.map! { |d| d.shuffle(random.Random.new(key)) }
+    arr.flatten!
+    document = [] # new array to store original doc
+    counter = 1
+    arr.each do
+      document << ciphertext[arr.index(counter)]
+      counter += 1
+    end
+    document.join
   end
 end
