4d1f3ec221f3cebd320e826c2b6bb9e84478077b
tnfontenelle <tnfontenelle@hotmail.com>
2015-04-08 22:27:52 +0800

    Formatting changes to double transposition Cipher

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -8,14 +8,17 @@ module DoubleTranspositionCipher
     # 3. sort rows in predictibly random way using key as seed
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
+
     document = document.to_s # converting the document to a string
     squarert = Math.sqrt(document.length)
+
     row = squarert.round # finding number of rows & rounding it up
     col = 0
     squarert == squarert.to_i ? col = row : col = (document.length / row) + 1
 
     # breaking up doc into a matrix that is almost a square
     document = document.chars.to_a.each_slice(col).to_a
+
     # randomly shuffling rows first, then columns within rows
     document.shuffle!(random: Random.new(key))
     document.map! { |d| d.shuffle(random: Random.new(key)) }.join
@@ -28,17 +31,22 @@ module DoubleTranspositionCipher
     row = squarert.round
     col = 0
     squarert == squarert.to_i ? col = row : col = (ciphertext.length / row) + 1
+
     # breaking up arr into a matrix that is almost a square
     arr = (1..ciphertext.length).to_a.each_slice(col).to_a
+
     # randomly shuffling rows first, then columns within rows
     arr.shuffle!(random: Random.new(key))
     arr.map! { |d| d.shuffle(random: Random.new(key)) }.flatten!
+
     document = [] # new array to store original doc
     counter = 1
     arr.each do
       document << ciphertext[arr.index(counter)]
       counter += 1
     end
+
     document.join
   end
+
 end
